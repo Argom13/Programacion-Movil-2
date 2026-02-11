f3896db2e56cb2f2f991ac81396e45def57077c2
Argom13 <diegoandresfajardoromero@gmail.com>
2026-02-10 18:02:31 -0600

    Clase 10/2/2026

---
+++ CambioColor/CambioColor/AppShell.xaml.cs
@@ -0,0 +1,10 @@
+﻿namespace CambioColor
+{
+    public partial class AppShell : Shell
+    {
+        public AppShell()
+        {
+            InitializeComponent();
+        }
+    }
+}
---
+++ CambioColor/CambioColor/GlobalXmlns.cs
@@ -0,0 +1,2 @@
+[assembly: XmlnsDefinition("http://schemas.microsoft.com/dotnet/maui/global", "CambioColor")]
+[assembly: XmlnsDefinition("http://schemas.microsoft.com/dotnet/maui/global", "CambioColor.Pages")]
