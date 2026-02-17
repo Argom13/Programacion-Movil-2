1251643380ee1326f680ebd80ac2c42c4134ac55
Argom13 <diegoandresfajardoromero@gmail.com>
2026-02-16 18:03:48 -0600

    Ejercicio 1 segundo Parcial 16/2/2026

---
+++ PropinaMAUI/PropinaMAUI/AppShell.xaml.cs
@@ -0,0 +1,10 @@
+﻿namespace PropinaMAUI
+{
+    public partial class AppShell : Shell
+    {
+        public AppShell()
+        {
+            InitializeComponent();
+        }
+    }
+}
---
+++ PropinaMAUI/PropinaMAUI/MainPage.xaml.cs
@@ -0,0 +1,14 @@
+﻿namespace PropinaMAUI
+{
+    public partial class MainPage : ContentPage
+    {
+        
+
+        public MainPage()
+        {
+            InitializeComponent();
+        }
+
+        
+    }
+}
