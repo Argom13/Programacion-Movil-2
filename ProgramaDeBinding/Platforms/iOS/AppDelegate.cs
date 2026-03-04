901aff2694e220f4518ff2e3cd8a219807975e40
juandi022 <juandiandradeflores@gmail.com>
2026-03-03 18:13:56 -0600

    Archivo de Hoy

---
+++ ProgramaDeBinding/Platforms/iOS/AppDelegate.cs
@@ -0,0 +1,10 @@
+﻿using Foundation;
+
+namespace ProgramaDeBinding
+{
+    [Register("AppDelegate")]
+    public class AppDelegate : MauiUIApplicationDelegate
+    {
+        protected override MauiApp CreateMauiApp() => MauiProgram.CreateMauiApp();
+    }
+}
---
+++ ProgramaDeBinding/modelo/person.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ProgramaDeBinding.modelo
+{
+    public class Person
+    {
+        public string Name { get; set; }
+        public string Phone { get; set; }
+        public string Address { get; set; }
+    }
+
+}
